0c12376f6d92bb5b20de4dfe7cf72c8aba9ad1ba
RafaRossi <rossi.rafael.proenca@gmail.com>
2022-07-16 08:58:06 -0300

    First Commit

---
+++ test.ts
@@ -0,0 +1,21 @@
+﻿class Student
+{
+    constructor(name: string, age: number, isGraduated: boolean) {
+        this.name = name;
+        this.age = age;
+        this.isGraduate = isGraduated;
+    }
+    
+    private name: string = '';
+    private age: number = 0;
+    private isGraduate: boolean = false;
+    
+    public studentFormattedData() : string
+    {
+        return 'Nome: ' + this.name + ', Idade: ' + this.age;
+    }
+}
+
+const student = new Student('Rafael', 24, true);
+
+console.log(student.studentFormattedData())
