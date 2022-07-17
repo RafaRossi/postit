7ad303260597509c5d104794c9346029b5038b91
RafaRossi <rossi.rafael.proenca@gmail.com>
2022-07-17 15:07:06 -0300

    update(navbar): navbar with for loop

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {NavbarEnum} from "../../models/enums/navbar.enum";
 import {NavigationEnd, Router} from "@angular/router";
 import {filter} from "rxjs/operators";
+import {NavbarItemInterface} from "../../models/interface/navbar-item.interface";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent{
 
   constructor(
     private readonly router: Router,
@@ -27,11 +28,26 @@ export class NavbarComponent implements OnInit {
       });
   }
 
-
-  public navBarEnum: typeof NavbarEnum = NavbarEnum;
   public currentNavbar: NavbarEnum = NavbarEnum.HOME;
 
-  ngOnInit() {
-
-  }
+  public navbarList: NavbarItemInterface[] = [
+    {
+      type: NavbarEnum.FEED,
+      link: '/feed',
+      icon: "assets/images/calendar-off.svg",
+      iconActivated: "assets/images/calendar-on.svg",
+    },
+    {
+      type: NavbarEnum.HOME,
+      link: '/home',
+      icon: "assets/images/home-off.svg",
+      iconActivated: "assets/images/home-on.svg",
+    },
+    {
+      type: NavbarEnum.PROFILE,
+      link: '/profile',
+      icon: "assets/images/user-off.svg",
+      iconActivated: "assets/images/user-on.svg",
+    }
+  ]
 }
---
+++ src/app/models/interface/navbar-item.interface.ts
@@ -0,0 +1,9 @@
+﻿import {NavbarEnum} from "../enums/navbar.enum";
+
+export interface NavbarItemInterface
+{
+  type: NavbarEnum
+  link: string,
+  icon: string,
+  iconActivated: string,
+}
